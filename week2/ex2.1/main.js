875ce88ef5bc88019f39e4e39fbba217f6e3ac06
Simon <simon.sharaya@gmail.com>
2024-01-24 19:09:27 -0600

    ex2 done

---
+++ week2/ex2.1/main.js
@@ -0,0 +1,4 @@
+const movielist = ["Interstellar", "Shutter Island", "NoBody", "Knives Out"];
+const yourmovie= window.prompt("what is your favorite movie?")
+movielist.push(yourmovie);
+console.log(movielist);
